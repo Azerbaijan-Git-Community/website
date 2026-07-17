52290285517c3c8fa0577cb312d34a47175f8375
Yusif Aliyev <yusifaliyevpro@gmail.com>
2026-07-17 10:45:19 +0400

    fix: update oxlint configuration to include additional plugins and adjust import rules

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "oxlint";
 
 export default defineConfig({
-  plugins: ["typescript", "nextjs", "unicorn", "import"],
+  plugins: ["typescript", "nextjs", "unicorn", "import", "jsx-a11y", "react", "react-perf"],
   categories: {
     suspicious: "warn",
   },
@@ -9,8 +9,12 @@ export default defineConfig({
     eqeqeq: "warn",
     "no-underscore-dangle": ["warn", { allow: ["_count", "_sum", "_avg", "_min", "_max"] }],
     "no-throw-literal": "warn",
-    "import/no-unassigned-import": "off",
+    "import/no-unassigned-import": [
+      "warn",
+      { allow: ["**/globals.css", "**/env.server", "dotenv/config", "server-only"] },
+    ],
     "unicorn/prefer-node-protocol": "warn",
+    "react/react-in-jsx-scope": "off",
     "typescript/consistent-type-imports": "warn",
   },
 });
